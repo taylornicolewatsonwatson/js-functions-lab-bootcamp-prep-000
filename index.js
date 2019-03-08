f2d5b40dde724e3423638d004f318e3dc201e942
Taylor Watson <taylortwatson@gmail.com>
2019-03-08 22:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function happyHolidays(string){ return "Happy holidays!" }
 function happyHolidaysTo(name){ return `Happy holidays, ${name}!` }
 function happyHolidaysTo(holiday, name){ return `Happy ${holiday}, ${name}!`}
-function holidayCountdown(holiday, days){return `It's ${days} days until ${holiday}`}
+function holidayCountdown(holiday, days){return `It's ${days} days until ${holiday}!`}
